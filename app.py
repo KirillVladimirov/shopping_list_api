8728bdae91825f9dd29b73baab982419b4263e0a
Kirill Vladimirov <kvladimirov.main@gmail.com>
2018-02-26 13:53:50 +0300

    create admin

--- app.py
+++ app.py
@@ -6,62 +6,82 @@ from flask import (
     make_response,
     request,
 )
+from flask_pymongo import PyMongo
 
-tasks = [
+items = [
     {
         'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'title': 'Milk',
+        'description': '',
         'done': False
     },
     {
         'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
+        'title': 'Cheese',
+        'description': 'for pizza',
         'done': False
     },
     {
         "id": 3,
-        "title": "new item 1",
-        "description": "new description 1",
+        "title": "Pizza",
+        "description": "pepperoni",
+        "done": False,
+    },
+    {
+        "id": 4,
+        "title": "Fruit",
+        "description": "apple",
+        "done": False,
+    },
+    {
+        "id": 5,
+        "title": "Meat",
+        "description": "beef",
         "done": False,
     }
 ]
 
 app = Flask(__name__)
+mongo = PyMongo(app)
+
+
+@app.route('/shopping/admin', methods=['GET'])
+def home_page():
+    shopping_list = mongo.db.shopping.find({'online': True})
+    return jsonify({'items': shopping_list})
 
 
 @app.route('/shopping/list', methods=['GET'])
 def get_list():
-    return jsonify({'tasks': tasks})
+    return jsonify({'items': items})
 
 
 @app.route('/shopping/list/<int:item_id>', methods=['GET'])
 def get_item(item_id):
-    task = list(filter(lambda t: t['id'] == item_id, tasks))
-    if len(task) == 0:
+    item = list(filter(lambda t: t['id'] == item_id, items))
+    if len(item) == 0:
         abort(404)
-    return jsonify({'task': task[0]})
+    return jsonify({'items': item[0]})
 
 
 @app.route('/shopping/list', methods=['POST'])
-def create_task():
+def create_item():
     if not request.json or 'title' not in request.json:
         abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
+    item = {
+        'id': items[-1]['id'] + 1,
         'title': request.json['title'],
         'description': request.json.get('description', ""),
         'done': False
     }
-    tasks.append(task)
-    return jsonify({'task': task}), 201
+    items.append(item)
+    return jsonify({'items': item}), 201
 
 
 @app.route('/shopping/list/<int:item_id>', methods=['PUT'])
 def update_item(item_id):
-    task = list(filter(lambda t: t['id'] == item_id, tasks))
-    if len(task) == 0:
+    item = list(filter(lambda t: t['id'] == item_id, items))
+    if len(item) == 0:
         abort(404)
     if not request.json:
         abort(400)
@@ -71,18 +91,18 @@ def update_item(item_id):
         abort(400)
     if 'done' not in request.json:
         abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': task[0]})
+    item[0]['title'] = request.json.get('title', item[0]['title'])
+    item[0]['description'] = request.json.get('description', item[0]['description'])
+    item[0]['done'] = request.json.get('done', item[0]['done'])
+    return jsonify({'items': item[0]})
 
 
 @app.route('/shopping/list/<int:item_id>', methods=['DELETE'])
 def delete_item(item_id):
-    task = list(filter(lambda t: t['id'] == item_id, tasks))
-    if len(task) == 0:
+    item = list(filter(lambda t: t['id'] == item_id, items))
+    if len(item) == 0:
         abort(404)
-    tasks.remove(task[0])
+        items.remove(item[0])
     return jsonify({'result': True})
 
 
